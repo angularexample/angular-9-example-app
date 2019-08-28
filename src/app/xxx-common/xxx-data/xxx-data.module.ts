d8dc930a76afac30296b0f6370957332d032adc5
AngularExample <info@angularexample.com>
2019-08-28 19:06:53 -0400

    data module cleanup

--- src/app/xxx-common/xxx-data/xxx-data.module.ts
+++ src/app/xxx-common/xxx-data/xxx-data.module.ts
@@ -10,12 +10,5 @@ import {XxxDataResponseInterceptor} from '../xxx-data-response-interceptor/xxx-d
   ]
 })
 
-/**
- * NOTE: After changing all services to use providedIn,
- * it is necessary to import this module at the app module,
- * or else in any module that uses the data service.
- * Without it, you will get run time error no provider for HttpClient and HttpHandler.
- * You also need this to bind the interceptor to the data service.
- */
 export class XxxDataModule {
 }
